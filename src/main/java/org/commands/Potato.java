40e027d9ca623c220513a9baf3a58df4f4f483ed
noahggarrett <noahggarrett@protonmail.com>
2021-01-23 05:31:40 -0700

    Added firework command

--- src/main/java/org/commands/Potato.java
+++ src/main/java/org/commands/Potato.java
@@ -1,6 +1,7 @@
 package org.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.command.SimpleCommand;
@@ -16,5 +17,7 @@ public class Potato extends SimpleCommand {
 		Player player = getPlayer();
 		player.sendMessage("potato is a god!");
 		Common.tell(player, "&9Person is better though");
+		player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK); // firewords
+
 	}
 }
